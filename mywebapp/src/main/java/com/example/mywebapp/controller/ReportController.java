8874e8b4d34d254468a67fd34ca129831ee0e88e
KumarageKPV <it23178540@my.sliit.lk>
2024-11-26 23:20:54 +0530

    Committed by Pavan (3)

--- mywebapp/src/main/java/com/example/mywebapp/controller/ReportController.java
+++ mywebapp/src/main/java/com/example/mywebapp/controller/ReportController.java
@@ -8,6 +8,7 @@ import com.example.mywebapp.model.Juice;
 import com.example.mywebapp.model.Supplier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")  // Allow requests from the frontend's origin (update accordingly)
 @RequestMapping("/api/reports")
 public class ReportController {
 
