742a55fe58ea6f7ebf372cbec6c60688ee8fe2d8
KumarageKPV <it23178540@my.sliit.lk>
2024-11-26 23:44:48 +0530

    Committed by Pavan (4)

--- mywebapp/src/main/java/com/example/mywebapp/controller/ReportController.java
+++ mywebapp/src/main/java/com/example/mywebapp/controller/ReportController.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")  // Allow requests from the frontend's origin (update accordingly)
+@CrossOrigin(origins = "http://localhost:00")  // Allow requests from the frontend's origin (update accordingly)
 @RequestMapping("/api/reports")
 public class ReportController {
 
